7b93ec9539570712c1c04bcc3ca00b740081bd00
sfad159357 <sfad159357@gmail.com>
2020-08-15 15:18:29 +0800

    routes cors3

--- startup/routes.js
+++ startup/routes.js
@@ -22,12 +22,15 @@ module.exports = function (app) {
   });
 
   app.use(express.json());
+  app.get("/api/genres", genres);
   app.use("/api/genres", genres);
   app.use("/api/customers", customers);
   app.use("/api/movies", movies);
+  app.get("/api/movies", movies);
   app.use("/api/rentals", rentals);
   app.use("/api/users", users);
   app.use("/api/auth", auth);
+  app.get("/api/auth", auth);
   app.use("/api/returns", returns);
   app.use(error);
 };

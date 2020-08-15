09f0cb5414da39faf05f146cfc9f4423ef3bbe2d
sfad159357 <sfad159357@gmail.com>
2020-08-15 15:09:00 +0800

    routes cors1 back

--- startup/routes.js
+++ startup/routes.js
@@ -9,15 +9,6 @@ const returns = require("../routes/returns");
 const error = require("../middleware/error");
 
 module.exports = function (app) {
-  app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
-
   app.use(express.json());
   app.use("/api/genres", genres);
   app.use("/api/customers", customers);

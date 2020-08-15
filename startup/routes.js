3a96d334dd9bd7bb8a99ed8ad90e6f897c92018c
sfad159357 <sfad159357@gmail.com>
2020-08-15 15:03:26 +0800

    routes cors1

--- startup/routes.js
+++ startup/routes.js
@@ -1,21 +1,30 @@
-const express = require('express');
-const genres = require('../routes/genres');
-const customers = require('../routes/customers');
-const movies = require('../routes/movies');
-const rentals = require('../routes/rentals');
-const users = require('../routes/users');
-const auth = require('../routes/auth');
-const returns = require('../routes/returns');
-const error = require('../middleware/error');
+const express = require("express");
+const genres = require("../routes/genres");
+const customers = require("../routes/customers");
+const movies = require("../routes/movies");
+const rentals = require("../routes/rentals");
+const users = require("../routes/users");
+const auth = require("../routes/auth");
+const returns = require("../routes/returns");
+const error = require("../middleware/error");
 
-module.exports = function(app) {
+module.exports = function (app) {
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+
   app.use(express.json());
-  app.use('/api/genres', genres);
-  app.use('/api/customers', customers);
-  app.use('/api/movies', movies);
-  app.use('/api/rentals', rentals);
-  app.use('/api/users', users);
-  app.use('/api/auth', auth);
-  app.use('/api/returns', returns);
+  app.use("/api/genres", genres);
+  app.use("/api/customers", customers);
+  app.use("/api/movies", movies);
+  app.use("/api/rentals", rentals);
+  app.use("/api/users", users);
+  app.use("/api/auth", auth);
+  app.use("/api/returns", returns);
   app.use(error);
-}
+};

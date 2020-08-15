8feb1ff269ea60fc070523cea3e42218c47c36f5
sfad159357 <sfad159357@gmail.com>
2020-08-15 15:14:53 +0800

    routes cors2

--- startup/routes.js
+++ startup/routes.js
@@ -9,6 +9,18 @@ const returns = require("../routes/returns");
 const error = require("../middleware/error");
 
 module.exports = function (app) {
+  app.use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Origin",
+      "safe-waters-08794.herokuapp.com"
+    ); // update to match the domain you will make the request from
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+
   app.use(express.json());
   app.use("/api/genres", genres);
   app.use("/api/customers", customers);

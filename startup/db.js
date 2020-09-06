330b234bd35a0f649a53d6f6b5a00f2921a3cfc8
sfad159357 <sfad159357@gmail.com>
2020-09-06 18:39:09 +0800

    新增mongodb模組.Client

--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,18 @@
-const winston = require('winston');
-const mongoose = require('mongoose');
-const config = require('config');
+const winston = require("winston");
+const mongoose = require("mongoose");
+const config = require("config");
+const MongoClient = require("mongodb").MongoClient;
 
-module.exports = function() {
-  const db = config.get('db');
-  mongoose.connect(db)
-    .then(() => winston.info(`Connected to ${db}...`));
-}
+module.exports = function () {
+  const uri =
+    "mongodb+srv://sfad159357:753951sfad@cluster0.rjrvl.gcp.mongodb.net/vidly_db?retryWrites=true&w=majority";
+  const client = new MongoClient(uri, { useNewUrlParser: true });
+  client.connect((err) => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+  });
+
+  const db = config.get("db");
+  mongoose.connect(db).then(() => winston.info(`Connected to ${db}...`));
+};

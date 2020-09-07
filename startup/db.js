15ba228b644343604133732c7262a3e1d1778097
sfad159357 <sfad159357@gmail.com>
2020-09-07 11:42:27 +0800

    7

--- startup/db.js
+++ startup/db.js
@@ -1,17 +1,17 @@
 const winston = require("winston");
 const mongoose = require("mongoose");
 const config = require("config");
-const MongoClient = require("mongodb").MongoClient;
+// const MongoClient = require("mongodb").MongoClient;
 
 module.exports = function () {
-  var uri =
-    " mongodb://sfad159357:753951sfad@cluster0-shard-00-00.rjrvl.gcp.mongodb.net:27017,cluster0-shard-00-01.rjrvl.gcp.mongodb.net:27017,cluster0-shard-00-02.rjrvl.gcp.mongodb.net:27017/vidly_db?ssl=true&replicaSet=atlas-brw5hw-shard-0&authSource=admin&retryWrites=true&w=majority";
+  // var uri =
+  //   " mongodb://sfad159357:753951sfad@cluster0-shard-00-00.rjrvl.gcp.mongodb.net:27017,cluster0-shard-00-01.rjrvl.gcp.mongodb.net:27017,cluster0-shard-00-02.rjrvl.gcp.mongodb.net:27017/vidly_db?ssl=true&replicaSet=atlas-brw5hw-shard-0&authSource=admin&retryWrites=true&w=majority";
 
-  MongoClient.connect(uri, function (err, client) {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-  });
+  // MongoClient.connect(uri, function (err, client) {
+  //   const collection = client.db("test").collection("devices");
+  //   // perform actions on the collection object
+  //   client.close();
+  // });
 
   const db = config.get("db");
   mongoose.connect(db).then(() => winston.info(`Connected to ${db}...`));

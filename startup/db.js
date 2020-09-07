0865206e498508251e2245798f1736945385ca9b
sfad159357 <sfad159357@gmail.com>
2020-09-07 10:58:09 +0800

    4

--- startup/db.js
+++ startup/db.js
@@ -5,7 +5,7 @@ const MongoClient = require("mongodb").MongoClient;
 
 module.exports = function () {
   var uri =
-    "mongodb://sfad159357:753951sfad@cluster0-shard-00-00.rjrvl.gcp.mongodb.net:27017/vidly_db?retryWrites=true&ssl=true&authSource=admin";
+    "mongodb://sfad159357:753951sfad@cluster0-shard-00-00.rjrvl.gcp.mongodb.net:27017/vidly_db?retryWrites=true&ssl=true&authSource=admin,replicaSetHost:replicaSetPort/vidly_db?replicaSet=rs-someServer";
   MongoClient.connect(uri, function (err, client) {
     const collection = client.db("test").collection("devices");
     // perform actions on the collection object

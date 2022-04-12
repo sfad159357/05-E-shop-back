055811c99fc3de4f895c7a67d5ba66d369c30da7
簡名彥 <chieng-ming-yen@jianmingyande-MacBook-Pro.local>
2022-04-12 14:33:37 +0800

    db.js加一些參數2

--- startup/db.js
+++ startup/db.js
@@ -14,6 +14,6 @@ module.exports = function () {
   // });
 
   const db = config.get("db");
-  mongoose.connect(db).then(() => winston.info(`Connected to ${db}...`),
+  mongoose.connect(db,
    { useNewUrlParser: true, useUnifiedTopology: true });
 };
